ed5a37a7fb00899798b0c05163a56bd5deec07a2
Marek Bełz <marazm.lab@gmail.com>
2025-08-09 13:01:26 +0000

    feat: added addTask function to render input in list

--- dist/app.js
+++ dist/app.js
@@ -2,6 +2,8 @@
 //   <label for="task1">Feed dog</label>
 //   <input type="checkbox" id="task-1" name="Feed dog" />
 // </li>;
+const taskNameInputElement = document.querySelector("#name");
+const addButtonElement = document.querySelector("button");
 const tasksContainerElement = document.querySelector(".tasks");
 const tasks = [
     "Feed the dog",
@@ -10,10 +12,19 @@ const tasks = [
     "Fix the cabinet",
 ];
 const render = () => {
+    tasksContainerElement.innerHTML = "";
     tasks.forEach((task) => {
         const taskElement = document.createElement("li");
         taskElement.innerText = task;
         tasksContainerElement.appendChild(taskElement);
     });
 };
+const addTask = (task) => {
+    tasks.push(task);
+};
+addButtonElement.addEventListener("click", (event) => {
+    event.preventDefault();
+    tasks.push(taskNameInputElement.value);
+    render();
+});
 render();
--- src/app.ts
+++ src/app.ts
@@ -3,6 +3,8 @@
 //   <input type="checkbox" id="task-1" name="Feed dog" />
 // </li>;
 
+const taskNameInputElement: HTMLInputElement = document.querySelector("#name");
+const addButtonElement: HTMLButtonElement = document.querySelector("button");
 const tasksContainerElement: HTMLElement = document.querySelector(".tasks");
 
 const tasks: string[] = [
@@ -13,11 +15,22 @@ const tasks: string[] = [
 ];
 
 const render = () => {
+  tasksContainerElement.innerHTML = "";
   tasks.forEach((task) => {
     const taskElement: HTMLElement = document.createElement("li");
     taskElement.innerText = task;
     tasksContainerElement.appendChild(taskElement);
   });
 };
+
+const addTask = (task: string) => {
+  tasks.push(task);
+};
+
+addButtonElement.addEventListener("click", (event: Event) => {
+  event.preventDefault();
+  tasks.push(taskNameInputElement.value);
+  render();
+});
 
 render();
