45bbb222a7a0a26edf0389886afce9ec7c660cc8
Marek Bełz <marazm.lab@gmail.com>
2025-08-15 14:38:29 +0000

    chore: app.js modified

--- dist/app.js
+++ dist/app.js
@@ -54,4 +54,3 @@ addTask({ name: taskName, category: taskCategory, done: taskDoneStatus });
 renderCategories(categories, categoriesContainerelement, updateSelectedCategory);
 renderTasks(tasks, tasksContainerElement);
 const taskClassInstance = new TaskClass("contructor task", false);
-console.log(taskClassInstance.createdAt);
